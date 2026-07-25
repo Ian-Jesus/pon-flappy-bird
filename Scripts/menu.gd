d87b6739d06666d5388ee16cc34179a14ff547b6
Ian Jesus <Ianjesusrodriguezmartinez2@gmail.com>
2026-07-25 14:12:20 -0300

    Hice un menu inicial

---
+++ Scripts/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+func _on_iniciar_pressed() -> void:
+	# get_tree().change_scene_to_file()
+	pass # Replace with function body.
+
+
+func _on_opciones_pressed() -> void:
+	# get_tree().change_scene_to_file()
+	pass # Replace with function body.
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
